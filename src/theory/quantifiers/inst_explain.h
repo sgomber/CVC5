3443084d616422f01d99bf04b33e9c6deafb4725
ajreynol <andrew.j.reynolds@gmail.com>
2019-03-31 22:38:18 -0500

    Minor

--- src/theory/quantifiers/inst_explain.h
+++ src/theory/quantifiers/inst_explain.h
@@ -2,7 +2,7 @@
 /*! \file inst_explain.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Andrew Reynolds, Mathias Preiner, Morgan Deters
+ **   Andrew Reynolds
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
