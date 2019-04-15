95d99987a494e2a2896403e9b6d517424cea83be
ajreynol <andrew.j.reynolds@gmail.com>
2019-04-14 22:02:17 -0500

    Decide true algo

--- src/theory/quantifiers/inst_explain.cpp
+++ src/theory/quantifiers/inst_explain.cpp
@@ -128,6 +128,132 @@ int IeEvaluator::evaluateInternal(
 
 bool IeEvaluator::ensureValue(Node n, bool isTrue, std::map<Node,int>& setAssumps)
 {
+  std::unordered_set<Node, NodeHashFunction> ucache;
+  // if possible, propagate the literal in the clause that must be true
+  std::map< bool, std::unordered_set<Node, NodeHashFunction> > visited;
+  std::vector<TNode> visit;
+  std::vector<bool> visitE;
+  TNode cur;
+  bool curReq;
+  visit.push_back(n);
+  visitE.push_back(isTrue);
+  do
+  {
+    cur = visit.back();
+    visit.pop_back();
+    curReq = visitE.back();
+    visitE.pop_back();
+    int evCur = evaluateInternal(cur,d_ecache,ucache,false);
+    if( evCur!=0 )
+    {
+      if( (evCur==1)!=curReq )
+      {
+        // already wrong, we fail
+        return false;
+      }
+      // already correct, nothing to do
+    }
+    else if (visited[curReq].find(cur) == visited[curReq].end())
+    {
+      visited[curReq].insert(cur);
+      Kind k = cur.getKind();
+      if( k==NOT )
+      {
+        visit.push_back(cur[0]);
+        visitE.push_back(!curReq);
+      }
+      else if (k == AND || k == OR)
+      {
+        if ((k == AND) == curReq)
+        {
+          // all are required
+          for(TNode cc : cur)
+          {
+            visit.push_back(cc);
+            visitE.push_back(curReq);
+          }
+        }
+        else
+        {
+          // find one whose value is unknown
+          for(TNode cc : cur)
+          {
+            int cres = evaluateInternal(cc,d_ecache,ucache,false);
+            if (cres == 0)
+            {
+              // if one child is unknown, then we use it
+              visit.push_back(cc);
+              visitE.push_back(curReq);
+              break;
+            }
+          }
+        }
+      }
+      else if (k == ITE || (k == EQUAL && cur[0].getType().isBoolean()))
+      {
+        int ev0 = evaluateInternal(cur[0],d_ecache,ucache,false);
+        if( ev0!=0 )
+        {
+          // implies a single requirement
+          int index = (k==ITE && ev0==-1) ? 2 : 1;
+          bool reqc = (k!=ITE && ev0==-1) ? !curReq : curReq;
+          visit.push_back(cur[index]);
+          visitE.push_back(reqc);
+        }
+        else if( k==ITE )
+        {
+          // find a branch that is not wrong
+          int processIndex = -1;
+          bool processIndexUnk = false;
+          for( unsigned i=1; i<=2; i++ )
+          {
+            int evi = evaluateInternal(cur[i],d_ecache,ucache,false);
+            if( (evi==1)==curReq )
+            {
+              processIndex = i;
+              processIndexUnk = false;
+              break;
+            }
+            else if( evi==0 )
+            {
+              processIndex = i;
+              processIndexUnk = true;
+            }
+          }
+          visit.push_back(cur[0]);
+          visitE.push_back(processIndex==1);
+          if( processIndexUnk )
+          {
+            visit.push_back(cur[processIndex]);
+            visitE.push_back(curReq);
+          }
+        }
+        else
+        {
+          int ev1 = evaluateInternal(cur[1],d_ecache,ucache,false);
+          if( ev1==0 )
+          {
+            // make both true
+            visit.push_back(cur[0]);
+            visitE.push_back(true);
+            visit.push_back(cur[1]);
+            visitE.push_back(true);
+          }
+          else
+          {
+            // make match
+            visit.push_back(cur[0]);
+            visitE.push_back((ev1==1)==curReq);
+          }
+        }
+      }
+      else
+      {
+        setAssumps[cur] = curReq ? 1 : -1;
+      }
+    }
+  } while (!visit.empty());
+  
   
   return false;
 }
@@ -391,6 +517,7 @@ bool InstExplainInst::justifyInternal(
   Trace("iex-debug") << "  " << on << std::endl;
   if (on == olitProp)
   {
+    // we are at the target, thus we succeed here
     return true;
   }
   // only safe to cache wrt on
