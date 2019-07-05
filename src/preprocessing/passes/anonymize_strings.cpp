81e60990c5b8c563a0b6f798b9a84a5f7e6ff726
ajreynol <andrew.j.reynolds@gmail.com>
2019-07-05 16:36:28 -0500

    Minor

--- src/preprocessing/passes/anonymize_strings.cpp
+++ src/preprocessing/passes/anonymize_strings.cpp
@@ -2,7 +2,7 @@
 /*! \file anonymize_strings.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Caleb Donovick
+ **   Andres Noetzli, Andrew Reynolds
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
