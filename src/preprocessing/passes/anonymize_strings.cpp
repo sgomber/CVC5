1e1f23260b7f4f58f4a4c9e989a18eaf3490fc73
ajreynol <andrew.j.reynolds@gmail.com>
2019-07-21 12:35:34 -0500

    Term convert version

--- src/preprocessing/passes/anonymize_strings.cpp
+++ src/preprocessing/passes/anonymize_strings.cpp
@@ -428,33 +428,33 @@ bool solveAnonStrGraph(
 
 /// ---------------------------------------------------------------
 
-void collectLits(Node n,
+Node collectLits( Node n,
                  std::unordered_map<Node, Node, NodeHashFunction>* lits,
-                 std::unordered_set<Node, NodeHashFunction>* reranges)
-{
-  NodeManager* nm = NodeManager::currentNM();
-  std::unordered_map<TNode, bool, TNodeHashFunction> visited;
-  std::unordered_map<TNode, bool, TNodeHashFunction>::iterator it;
+                 std::unordered_set<Node, NodeHashFunction>* reranges ){
+  NodeManager * nm = NodeManager::currentNM();
+  std::unordered_map<TNode, Node, TNodeHashFunction> visited;
+  std::unordered_map<TNode, Node, TNodeHashFunction>::iterator it;
   std::vector<TNode> visit;
   TNode cur;
   visit.push_back(n);
-  do
+  do 
   {
     cur = visit.back();
     visit.pop_back();
     it = visited.find(cur);
-    if (it == visited.end())
-    {
-      visited[cur] = false;
 
-      visit.push_back(cur);
-      for (unsigned i = 0; i < cur.getNumChildren(); i++)
-      {
-        visit.push_back(cur[i]);
-      }
-    }
-    else if (!it->second)
+    if (it == visited.end()) 
     {
+      visited[cur] = Node::null();
+      visit.push_back(cur);
+      for (const Node& cn : cur )
+      {
+        visit.push_back(cn);
+      }
+    } 
+    else if (it->second.isNull()) 
+    {
+      Node ret = cur;
       if (cur.getKind() == kind::CONST_STRING)
       {
         if (cur.getConst<String>().size() > 0)
@@ -469,10 +469,31 @@ void collectLits(Node n,
       {
         reranges->insert(cur);
       }
-      visited[cur] = true;
+      bool childChanged = false;
+      std::vector<Node> children;
+      if (cur.getMetaKind() == kind::metakind::PARAMETERIZED) {
+        children.push_back(cur.getOperator());
+      }
+      for (const Node& cn : cur )
+      {
+        it = visited.find(cn);
+        Assert(it != visited.end());
+        Assert(!it->second.isNull());
+        childChanged = childChanged || cn != it->second;
+        children.push_back(it->second);
+      }
+      if (childChanged) 
+      {
+        ret = nm->mkNode(cur.getKind(), children);
+      }
+      visited[cur] = ret;
     }
   } while (!visit.empty());
+  Assert(visited.find(n) != visited.end());
+  Assert(!visited.find(n)->second.isNull());
+  return visited[n];
 }
+
 
 std::unordered_map<Node, std::vector<Node>, NodeHashFunction>
 computeContainsRels(
