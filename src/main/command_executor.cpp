11605813b1bb3d57a3fb7675cc32cecc3da2cc97
ajreynol <andrew.j.reynolds@gmail.com>
2019-11-05 15:25:32 -0600

    Do not dump models on unknown with TPTP output language.

--- src/main/command_executor.cpp
+++ src/main/command_executor.cpp
@@ -149,9 +149,11 @@ bool CommandExecutor::doCommandSingleton(Command* cmd)
   // dump the model/proof/unsat core if option is set
   if (status) {
     std::vector<std::unique_ptr<Command> > getterCommands;
+    // SMT-LIB says models are available when the result was sat or unknown.
+    // TPTP wants models when the result is sat.
     if (d_options.getProduceModels() && d_options.getDumpModels() &&
         (res.asSatisfiabilityResult() == Result::SAT ||
-         (res.isUnknown() && res.whyUnknown() == Result::INCOMPLETE))) {
+         (d_options.getOutputLanguage()!=language::output::LANG_TPTP && res.isUnknown() && res.whyUnknown() == Result::INCOMPLETE))) {
       getterCommands.emplace_back(new GetModelCommand());
     }
     if (d_options.getProof() && d_options.getDumpProofs() &&
