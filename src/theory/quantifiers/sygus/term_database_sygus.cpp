ca4a7eaacc4942c8e24d953e88afeb919c05a348
ajreynol <andrew.j.reynolds@gmail.com>
2020-01-31 12:03:21 -0600

    Copy branch

--- src/theory/quantifiers/sygus/term_database_sygus.cpp
+++ src/theory/quantifiers/sygus/term_database_sygus.cpp
@@ -955,6 +955,16 @@ Node TermDbSygus::evaluateBuiltin(TypeNode tn,
                                   const std::vector<Node>& args,
                                   bool tryEval)
 {
+  std::unordered_map<Node, Node, NodeHashFunction> visited;
+  return evaluateBuiltin(tn, bn, args, visited, tryEval);
+}
+Node TermDbSygus::evaluateBuiltin(
+    TypeNode tn,
+    Node bn,
+    const std::vector<Node>& args,
+    const std::unordered_map<Node, Node, NodeHashFunction>& visited,
+    bool tryEval)
+{
   if (args.empty())
   {
     return Rewriter::rewrite( bn );
@@ -971,7 +981,7 @@ Node TermDbSygus::evaluateBuiltin(TypeNode tn,
     // This may fail if there is a subterm of bn under the
     // substitution that is not constant, or if an operator in bn is not
     // supported by the evaluator
-    res = d_eval->eval(bn, varlist, args);
+    res = d_eval->eval(bn, varlist, args, visited);
   }
   if (res.isNull())
   {
