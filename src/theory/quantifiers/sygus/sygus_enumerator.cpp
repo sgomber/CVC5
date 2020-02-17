b9908268080aea21f1faf53fb305dece09b3aa1f
ajreynol <andrew.j.reynolds@gmail.com>
2020-02-16 22:41:36 -0600

    Fix

--- src/theory/quantifiers/sygus/sygus_enumerator.cpp
+++ src/theory/quantifiers/sygus/sygus_enumerator.cpp
@@ -343,7 +343,7 @@ bool SygusEnumerator::TermCache::addTerm(Node n)
     {
       ++(d_stats->d_enumTermsExampleEval);
       // Is it equivalent under examples?
-      Node bne = d_eec->addSearchVal(bnr);
+      Node bne = d_eec->addSearchVal(n, bnr);
       if (!bne.isNull())
       {
         if (bnr != bne)
