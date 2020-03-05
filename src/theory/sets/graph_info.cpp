528d5f89e7c71a72d70d49ab306d5d6c8fc3bdbe
ajreynol <andrew.j.reynolds@gmail.com>
2020-03-05 11:19:35 -0600

    More

---
+++ src/theory/sets/graph_info.cpp
@@ -0,0 +1,35 @@
+/*********************                                                        */
+/*! \file graph_info.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Implementation of data structures for the graph extension.
+ **/
+
+#include "theory/sets/graph_info.h"
+
+using namespace CVC4::kind;
+
+namespace CVC4 {
+namespace theory {
+namespace sets {
+
+void GraphInfo::addEdge(TNode src, TNode dst)
+{
+  EdgeInfo& ei = d_einfo[src][dst];
+  if (ei.d_id==0)
+  {
+    d_idCounter++;
+    ei.d_id = d_idCounter;
+  }
+}
+
+}  // namespace sets
+}  // namespace theory
+}  // namespace CVC4
---
+++ src/theory/sets/graph_info.h
@@ -0,0 +1,51 @@
+/*********************                                                        */
+/*! \file graph_info.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Reynolds
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief Data structures for the graph extension
+ **/
+
+#ifndef CVC4__THEORY__SETS__GRAPH_INFO_H
+#define CVC4__THEORY__SETS__GRAPH_INFO_H
+
+#include <unordered_map>
+
+#include "expr/node.h"
+
+namespace CVC4 {
+namespace theory {
+namespace sets {
+
+class EdgeInfo
+{
+ public:
+  EdgeInfo() : d_id(0) {}
+  /** Edge Id */
+  uint32_t d_id;
+};
+  
+class GraphInfo
+{
+ public:
+   GraphInfo() : d_idCounter(0){}
+   /** Add edge */
+   void addEdge(TNode src, TNode dst);
+   /** Domain */
+   std::unordered_map< TNode, std::unordered_map< TNode, EdgeInfo, TNodeHashFunction >, TNodeHashFunction > d_einfo;
+ private:  
+   /** Id counter? */
+   uint32_t d_idCounter;
+};
+
+}  // namespace sets
+}  // namespace theory
+}  // namespace CVC4
+
+#endif /* CVC4__THEORY__SETS__GRAPH_INFO_H */
