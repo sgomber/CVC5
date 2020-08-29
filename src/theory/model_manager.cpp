8dc741e55d49b20e7c101e2cc44e0e055d0d3a9c
ajreynol <andrew.j.reynolds@gmail.com>
2020-08-29 16:38:52 -0500

    Fix

--- src/theory/model_manager.cpp
+++ src/theory/model_manager.cpp
@@ -64,7 +64,6 @@ void ModelManager::finishInit(eq::EqualityEngineNotify* notify)
   }
   // notice that the equality engine of the model has yet to be assigned.
   initializeModelEqEngine(notify);
-  d_model->finishInit(d_modelEqualityEngine);
 
   // TODO inform the equality engine manager that we will be using notify
   // d_eem.usingModelNotify(notify);
