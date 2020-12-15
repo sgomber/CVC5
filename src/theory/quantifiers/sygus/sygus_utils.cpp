b29a4ba8accc2e9d73111ccade582da108803e7a
ajreynol <andrew.j.reynolds@gmail.com>
2020-12-15 15:31:20 -0600

    Fix

--- src/theory/quantifiers/sygus/sygus_utils.cpp
+++ src/theory/quantifiers/sygus/sygus_utils.cpp
@@ -40,19 +40,10 @@ Node SygusUtils::mkSygusConjecture(const std::vector<Node>& fs,
 {
   Assert(!fs.empty());
   NodeManager* nm = NodeManager::currentNM();
-<<<<<<< HEAD
-  std::vector<Node> ipls;
-  SygusAttribute ca;
-  Node sygusVar = nm->mkSkolem("sygus", nm->booleanType());
-  sygusVar.setAttribute(ca, true);
-  Node instAttr = nm->mkNode(INST_ATTRIBUTE, sygusVar);
-  ipls.push_back(instAttr);
-=======
   SygusAttribute ca;
   Node sygusVar = nm->mkSkolem("sygus", nm->booleanType());
   sygusVar.setAttribute(ca, true);
   std::vector<Node> ipls{nm->mkNode(INST_ATTRIBUTE, sygusVar)};
->>>>>>> 8c4598683e4edd217ed524d47c68a053b6881f4c
   // insert the remaining instantiation attributes
   ipls.insert(ipls.end(), iattrs.begin(), iattrs.end());
   Node ipl = nm->mkNode(INST_PATTERN_LIST, ipls);
