8315dcba28efff2d18ffa1fc74912abeb3f8bcc3
ajreynol <andrew.j.reynolds@gmail.com>
2021-04-28 15:48:22 -0500

    Sketching eliminate types preprocessing pass

---
+++ src/preprocessing/passes/elim_types.cpp
@@ -0,0 +1,77 @@
+/******************************************************************************
+ * Top contributors (to cur version):
+ *   Yoni Zohar, Andres Noetzli, Aina Niemetz
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The foreign_theory_rewrite preprocessing pass.
+ *
+ * Simplifies nodes of one theory using rewrites from another.
+ */
+
+#include "preprocessing/passes/foreign_theory_rewrite.h"
+
+#include "expr/node_traversal.h"
+#include "preprocessing/assertion_pipeline.h"
+#include "preprocessing/preprocessing_pass_context.h"
+#include "theory/rewriter.h"
+#include "theory/strings/arith_entail.h"
+
+namespace cvc5 {
+namespace preprocessing {
+namespace passes {
+
+using namespace cvc5::theory;
+ElimTypes::ElimTypes(PreprocessingPassContext* preprocContext)
+    : PreprocessingPass(preprocContext, "foreign-theory-rewrite"),
+      d_cache(preprocContext->getUserContext()){};
+
+void ElimTypes::collectTypes(const Node& n, std::unordered_set<TNode, TNodeHashFunction>& visited, std::map<TypeNode, std::vector<Node>>& syms)
+{
+  std::unordered_set<TNode, TNodeHashFunction>::iterator it;
+  std::vector<TNode> visit;
+  TNode cur;
+  visit.push_back(n);
+  do {
+    cur = visit.back();
+    visit.pop_back();
+    it = visited.find(cur);
+
+    if (it == visited.end()) {
+      visited.insert(cur);
+      if (cur.isVar())
+      {
+        syms[cur.getType()].push_back(cur);
+      }
+      else if (cur.getKind()==APPLY_UF)
+      {
+        visit.push_back(cur.getOperator());
+      }
+      visit.insert(visit.end(),cur.begin(),cur.end());
+    }
+  } while (!visit.empty());
+}
+
+PreprocessingPassResult ElimTypes::applyInternal(
+    AssertionPipeline* assertionsToPreprocess)
+{
+  std::unordered_set<TNode, TNodeHashFunction> visited;
+  std::map<TypeNode, std::vector<Node>> syms;
+  size_t nasserts = assertionsToPreprocess->size();
+  for (size_t i = 0; i < nasserts; ++i)
+  {
+    collectTypes((*assertionsToPreprocess)[i], visited, syms);
+  }
+
+  return PreprocessingPassResult::NO_CONFLICT;
+}
+
+}  // namespace passes
+}  // namespace preprocessing
+}  // namespace cvc5
---
+++ src/preprocessing/passes/elim_types.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__PREPROCESSING__PASSES__ELIM_TYPES_H
+#define CVC5__PREPROCESSING__PASSES__ELIM_TYPES_H
+
+#include "context/cdhashmap.h"
+#include "expr/node.h"
+#include "preprocessing/preprocessing_pass.h"
+
+namespace cvc5 {
+namespace preprocessing {
+namespace passes {
+
+class ElimTypes : public PreprocessingPass
+{
+  using NodeMap = context::CDHashMap<Node, Node, NodeHashFunction>;
+ public:
+  ElimTypes(PreprocessingPassContext* preprocContext);
+
+ protected:
+  PreprocessingPassResult applyInternal(
+      AssertionPipeline* assertionsToPreprocess) override;
+  /** Collect types */
+  void collectTypes(const Node& n, std::unordered_set<TNode, TNodeHashFunction>& visited);
+};
+
+}  // namespace passes
+}  // namespace preprocessing
+}  // namespace cvc5
+
+#endif /* CVC5__PREPROCESSING__PASSES__ELIM_TYPES_H */
