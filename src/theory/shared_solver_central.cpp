b1d124cbebfe65f50bb6be077124b9825b713bce
ajreynol <andrew.j.reynolds@gmail.com>
2021-07-02 13:57:46 -0500

    Add the shared solver for central

---
+++ src/theory/shared_solver_central.cpp
@@ -0,0 +1,133 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Shared solver for central equality engine architecture
+ */
+
+#include "theory/shared_solver_central.h"
+
+#include "theory/theory_engine.h"
+
+namespace cvc5 {
+namespace theory {
+
+SharedSolverCentral::SharedSolverCentral(TheoryEngine& te,
+                                         ProofNodeManager* pnm)
+    : SharedSolver(te, pnm), d_centralEe(nullptr)
+{
+}
+
+bool SharedSolverCentral::needsEqualityEngine(theory::EeSetupInfo& esi)
+{
+  return d_sharedTerms.needsEqualityEngine(esi);
+}
+
+void SharedSolverCentral::setEqualityEngine(eq::EqualityEngine* ee)
+{
+  d_sharedTerms.setEqualityEngine(ee);
+  d_centralEe = ee;
+}
+
+void SharedSolverCentral::preRegisterSharedInternal(TNode t)
+{
+  if (t.getKind() == kind::EQUAL)
+  {
+    // When sharing is enabled, we propagate from the shared terms manager also
+    d_sharedTerms.addEqualityToPropagate(t);
+  }
+}
+
+EqualityStatus SharedSolverCentral::getEqualityStatus(TNode a, TNode b)
+{
+  // if we're using a shared terms database, ask its status if a and b are
+  // shared.
+  if (d_sharedTerms.isShared(a) && d_sharedTerms.isShared(b))
+  {
+    if (d_sharedTerms.areEqual(a, b))
+    {
+      return EQUALITY_TRUE_AND_PROPAGATED;
+    }
+    else if (d_sharedTerms.areDisequal(a, b))
+    {
+      return EQUALITY_FALSE_AND_PROPAGATED;
+    }
+  }
+  // otherwise, ask the theory
+  return d_te.theoryOf(Theory::theoryOf(a.getType()))->getEqualityStatus(a, b);
+}
+
+TrustNode SharedSolverCentral::explain(TNode literal, TheoryId id)
+{
+  TrustNode texp;
+  Trace("shared-solver") << "SharedSolverCentral::explain: " << literal << " "
+                         << id << std::endl;
+  Assert(id == THEORY_BUILTIN || !Theory::usesCentralEqualityEngine(id));
+  if (id == THEORY_BUILTIN)
+  {
+    /*
+    // explanation based on the central equality engine, recursively?
+    std::vector<Node> assumptions;
+    std::vector<Node> toProcess;
+    toProcess.push_back(literal);
+    size_t index = 0;
+    while (index<toProcess.size())
+    {
+      Node lit = toProcess[index];
+      index++;
+      Trace("shared-solver") << "- exp " << lit << std::endl;
+      std::vector<TNode> currAssumptions;
+      d_centralEe->explainLit(lit, currAssumptions);
+      Trace("shared-solver") << "...got " << currAssumptions.size() <<
+    std::endl; if (currAssumptions.size()==1 && currAssumptions[0]==lit)
+      {
+        assumptions.push_back(lit);
+      }
+      else
+      {
+        for (TNode a : currAssumptions)
+        {
+          if (std::find(toProcess.begin(), toProcess.end(), a)==toProcess.end())
+          {
+            toProcess.push_back(a);
+          }
+        }
+      }
+    }
+    Node exp = NodeManager::currentNM()->mkAnd(assumptions);
+    */
+
+    Node exp = d_centralEe->mkExplainLit(literal);
+    texp = TrustNode::mkTrustPropExp(literal, exp, nullptr);
+    Trace("shared-solver")
+        << "\tTerm was propagated by THEORY_BUILTIN. Explanation: " << exp
+        << std::endl;
+  }
+  else
+  {
+    // By default, we ask the individual theory for the explanation.
+    // It is possible that a centralized approach could preempt this.
+    texp = d_te.theoryOf(id)->explain(literal);
+    Trace("shared-solver") << "\tTerm was propagated by owner theory: " << id
+                           << ". Explanation: " << texp.getNode() << std::endl;
+  }
+  return texp;
+}
+
+void SharedSolverCentral::assertSharedEquality(TNode equality,
+                                               bool polarity,
+                                               TNode reason)
+{
+  d_sharedTerms.assertEquality(equality, polarity, reason);
+}
+
+}  // namespace theory
+}  // namespace cvc5
---
+++ src/theory/shared_solver_central.h
@@ -0,0 +1,66 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Shared solver for central equality engine architecture
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__THEORY__SHARED_SOLVER_CENTRAL__H
+#define CVC5__THEORY__SHARED_SOLVER_CENTRAL__H
+
+#include "expr/node.h"
+#include "theory/shared_solver.h"
+
+namespace cvc5 {
+namespace theory {
+
+/**
+ * The shared solver in the test architecture.
+ */
+class SharedSolverCentral : public SharedSolver
+{
+ public:
+  SharedSolverCentral(TheoryEngine& te, ProofNodeManager* pnm);
+  virtual ~SharedSolverCentral() {}
+  //------------------------------------- initialization
+  /**
+   * Returns true if we need an equality engine, this has the same contract
+   * as Theory::needsEqualityEngine.
+   */
+  bool needsEqualityEngine(theory::EeSetupInfo& esi) override;
+  /** Set equality engine in the shared terms database */
+  void setEqualityEngine(eq::EqualityEngine* ee) override;
+  //------------------------------------- end initialization
+  /** Assert equality to the shared terms database. */
+  void assertSharedEquality(TNode equality,
+                            bool polarity,
+                            TNode reason) override;
+  /**
+   * Get equality status based on the equality engine of shared terms database
+   */
+  EqualityStatus getEqualityStatus(TNode a, TNode b) override;
+  /** Explain literal that was propagated by a theory or using shared terms
+   * database */
+  TrustNode explain(TNode literal, TheoryId id) override;
+
+ protected:
+  /** If t is an equality, add it as one that may be propagated */
+  void preRegisterSharedInternal(TNode t) override;
+  /** Pointer to the central equality engine */
+  eq::EqualityEngine* d_centralEe;
+};
+
+}  // namespace theory
+}  // namespace cvc5
+
+#endif /* CVC5__THEORY__SHARED_SOLVER_CENTRAL__H */
