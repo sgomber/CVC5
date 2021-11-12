2b1c19bec2339fc23e07a32a50a851090aea5821
ajreynol <andrew.j.reynolds@gmail.com>
2021-11-12 08:20:10 -0600

    Fix

--- src/preprocessing/passes/real_to_int.cpp
+++ src/preprocessing/passes/real_to_int.cpp
@@ -27,7 +27,6 @@
 #include "theory/theory_model.h"
 #include "util/rational.h"
 
-using namespace cvc5::kind;
 using namespace cvc5::theory;
 
 namespace cvc5 {
@@ -79,7 +78,6 @@ Node RealToInt::realToIntInternal(TNode n, NodeMap& cache, std::vector<Node>& va
               {
                 Assert(c.isConst());
                 coeffs.push_back(NodeManager::currentNM()->mkConst(
-                    CONST_RATIONAL,
                     Rational(c.getConst<Rational>().getDenominator())));
               }
             }
@@ -99,8 +97,7 @@ Node RealToInt::realToIntInternal(TNode n, NodeMap& cache, std::vector<Node>& va
               Node s;
               if (c.isNull())
               {
-                c = cc.isNull() ? NodeManager::currentNM()->mkConst(
-                        CONST_RATIONAL, Rational(1))
+                c = cc.isNull() ? NodeManager::currentNM()->mkConst(Rational(1))
                                 : cc;
               }
               else
@@ -134,15 +131,14 @@ Node RealToInt::realToIntInternal(TNode n, NodeMap& cache, std::vector<Node>& va
             }
             Node sumt =
                 sum.empty()
-                    ? NodeManager::currentNM()->mkConst(CONST_RATIONAL,
-                                                        Rational(0))
+                    ? NodeManager::currentNM()->mkConst(Rational(0))
                     : (sum.size() == 1
                            ? sum[0]
                            : NodeManager::currentNM()->mkNode(kind::PLUS, sum));
             ret = NodeManager::currentNM()->mkNode(
                 ret_lit.getKind(),
                 sumt,
-                NodeManager::currentNM()->mkConst(CONST_RATIONAL, Rational(0)));
+                NodeManager::currentNM()->mkConst(Rational(0)));
             if (!ret_pol)
             {
               ret = ret.negate();
@@ -176,7 +172,8 @@ Node RealToInt::realToIntInternal(TNode n, NodeMap& cache, std::vector<Node>& va
     else
     {
       TypeNode tn = n.getType();
-      if (tn.isReal())
+      // TODO will switch to just isReal
+      if (tn.isReal() && !tn.isInteger())
       {
         if (n.getKind() == kind::BOUND_VARIABLE)
         {
