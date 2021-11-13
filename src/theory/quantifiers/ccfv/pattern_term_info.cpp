405b300f4fc1a1d27f8eac95ca63b9c3d0759563
ajreynol <andrew.j.reynolds@gmail.com>
2021-11-13 16:25:08 -0600

    More

---
+++ src/theory/quantifiers/ccfv/pattern_term_info.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Info per pattern term in CCFV.
+ */
+
+#include "theory/quantifiers/pattern_term_info.h"
+
+namespace cvc5 {
+namespace theory {
+namespace quantifiers {
+
+PatTermInfo::PatTermInfo(context::Context* c, size_t numVars) : d_eq(c), d_numUnassignVar(c, numVars) {}
+
+bool isActive() const { return d_eq.isNull(); }
+
+}  // namespace quantifiers
+}  // namespace theory
+}  // namespace cvc5
---
+++ src/theory/quantifiers/ccfv/state.h
@@ -0,0 +1,120 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__THEORY__QUANTIFIERS__CCFV__INST_DRIVER_H
+#define CVC5__THEORY__QUANTIFIERS__CCFV__INST_DRIVER_H
+
+namespace cvc5 {
+namespace theory {
+namespace quantifiers {
+
+/**
+
+Q1: x : T1, y : T2
+Q2: y : T2, w : T2
+
+--- Q1
+x -> a
+  --- Q2
+  y -> b
+    w -> c
+    w -> d
+  y -> c
+    w -> d
+  y -> d
+  y -> e
+x -> b
+  y -> b
+  y -> e
+  y -> f
+
+*/
+class InstDriver : public QuantifiersModule
+{
+ public:
+  InstDriver(Env& env,
+                      QuantifiersState& qs,
+                      QuantifiersInferenceManager& qim,
+                      QuantifiersRegistry& qr,
+                      TermRegistry& tr);
+  /** Get quantifiers info */
+  QuantInfo& getQuantInfo(TNode q);
+  /** Get free variable info */
+  FreeVarInfo& getFreeVarInfo(TNode v);
+  /** Get pattern term info */
+  PatTermInfo& getPatTermInfo(TNode p);
+  /** Get equivalence class info */
+  EqcInfo& getEqcInfo(TNode r);
+
+ private:
+  /** are we finished? */
+  bool isFinished() const;
+  TNode getNextVariable();
+  /**
+   * Push variable v to the stack.
+   */
+  void pushVar(TNode v);
+  void popVar();
+
+  void assignVar(TNode v, TNode eqc);
+
+  bool eqNotifyTriggerPredicate(TNode predicate, bool value);
+  bool eqNotifyTriggerTermEquality(TheoryId tag,
+                                   TNode t1,
+                                   TNode t2,
+                                   bool value);
+
+  void eqNotifyConstantTermMerge(TNode t1, TNode t2);
+  void eqNotifyNewClass(TNode t);
+  void eqNotifyMerge(TNode t1, TNode t2);
+  void eqNotifyDisequal(TNode t1, TNode t2, TNode reason);
+
+  /**
+   * Called when it is determined what pattern p is equal to.
+   *
+   * If g is non-null, then g is the (ground) equivalence class that pattern p
+   * is equal to. If g is null, then we have determined that p will *never*
+   * merge into a ground equivalence class in this context.
+   */
+  void notifyPatternEqGround(TNode p, TNode g);
+  /**
+   * Called when the current watched match term was
+   */
+  void notifyQuantMatch(TNode q, bool success);
+
+  /** the set of ground equivalence classes */
+  NodeSet d_groundEqc;
+
+  /** The current stack of quantified variables */
+  std::vector<TNode> d_varStack;
+
+  /** The set of quantified formulas */
+  QuantifiersSet d_qset;
+  /** Map quantified formulas to their info */
+  std::map<Node, QuantInfo> d_quantInfo;
+  /** Free variable info */
+  std::map<Node, FreeVarInfo> d_fvInfo;
+  /** Pattern term info */
+  std::map<Node, PatTermInfo> d_pInfo;
+  /** Equivalence class info */
+  std::map<Node, EqcInfo> d_eqcInfo;
+};
+
+}  // namespace quantifiers
+}  // namespace theory
+}  // namespace cvc5
+
+#endif
