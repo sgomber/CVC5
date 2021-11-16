87fce7d47dfeec52a68be99eba99dc3eb3684e45
ajreynol <andrew.j.reynolds@gmail.com>
2021-11-16 16:29:02 -0600

    More

---
+++ src/theory/quantifiers/ccfv/search_level.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Information for each search level in ccfv
+ */
+
+#include "theory/quantifiers/ccfv/search_level.h"
+
+
+#include <sstream>
+
+namespace cvc5 {
+namespace theory {
+namespace quantifiers {
+namespace ccfv {
+
+SearchLevel::SearchLevel() : d_firstTime(true) {}
+
+std::string SearchLevel::toStringDebug() const
+{
+  std::stringstream ss;
+  ss << "Variables: " << d_varsToAssign << std::endl;
+  for (size_t i=0; i<2; i++)
+  {
+    const std::vector<TNode>& quants = i==0 ? d_startQuants : d_finalQuants;
+    ss << (i==0 ? "Start" : "Final") << " quants: [ ";
+    for (TNode q : quants)
+    {
+      ss << q.getId() << " ";
+    }
+    ss << "]" << std::endl;
+  }
+  return ss.str();
+}
+  
+}  // namespace ccfv
+}  // namespace quantifiers
+}  // namespace theory
+}  // namespace cvc5
+
