25d8a0bd5b456cdcb939489dbd1eaa16c5c1fc71
ajreynol <andrew.j.reynolds@gmail.com>
2022-03-04 12:55:09 -0600

    Sketching alpha equivalent variance

---
+++ src/expr/alpha_eq_variant_node_converter.cpp
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Implementation of subtype elimination node conversion
+ */
+
+#include "expr/alpha_eq_variant_node_converter.h"
+
+using namespace cvc5::kind;
+
+namespace cvc5 {
+
+AlphaEqVariantNodeConverter::AlphaEqVariantNodeConverter() {}
+
+Node AlphaEqVariantNodeConverter::postConvert(Node n)
+{
+  if (n.getKind()==BOUND_VARIABLE)
+  {
+    std::map<Node, Node>::iterator it = d_bvMap.find(n);
+    if (it!=d_bvMap.end())
+    {
+      return it->second;
+    }
+    Node v = NodeManager::currentNM()->mkBoundVar(n.getType());
+    d_bvMap[n] = v;
+    return v;
+  }
+  return n;
+}
+
+
+AlphaEqVariantProofGenerator::AlphaEqVariantProofGenerator(ProofNodeManager* pnm,
+                      context::Context* c,
+                      std::string name) : EagerProofGenerator(pnm, c, name)
+                      {
+                      }
+
+TrustNode AlphaEqVariantProofGenerator::convertEq(TrustNode eqt)
+{
+  
+  return eqt;
+}
+  
+}  // namespace cvc5
---
+++ src/expr/alpha_eq_variant_node_converter.h
@@ -0,0 +1,61 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Implementation of alpha equivalent variant node conversion
+ */
+#include "cvc5_private.h"
+
+#ifndef CVC4__PROOF__EXPR__ALPHA_EQ_VARIANT_NODE_CONVERTER_H
+#define CVC4__PROOF__EXPR__ALPHA_EQ_VARIANT_NODE_CONVERTER_H
+
+#include <iostream>
+#include <map>
+
+#include "expr/node.h"
+#include "expr/node_converter.h"
+#include "expr/type_node.h"
+#include "proof/eager_proof_generator.h"
+
+namespace cvc5 {
+
+/**
+ * This converts a node into an alpha equivalent node whose occurrences of
+ * BOUND_VARIABLE are fresh.
+ */
+class AlphaEqVariantNodeConverter : public NodeConverter
+{
+ public:
+  AlphaEqVariantNodeConverter();
+  ~AlphaEqVariantNodeConverter() {}
+  /** convert node n as described above during post-order traversal */
+  Node postConvert(Node n) override;
+ private:
+  /** Mapping bound variables to fresh bound variables of the same type */
+  std::map<Node, Node> d_bvMap;
+};
+
+/** 
+ * Proof-producing version of the above class. Stores 
+ */
+class AlphaEqVariantProofGenerator : public EagerProofGenerator
+{
+public:
+  AlphaEqVariantProofGenerator(ProofNodeManager* pnm,
+                      context::Context* c = nullptr,
+                      std::string name = "AlphaEqVariantProofGenerator");
+  /** Convert trust node */
+  TrustNode convertEq(TrustNode eqt);
+};
+
+}  // namespace cvc5
+
+#endif
