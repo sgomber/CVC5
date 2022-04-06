774bc8e4939b228dd908b3d7fcff6f7cd676c1bf
ajreynol <andrew.j.reynolds@gmail.com>
2022-04-06 09:13:18 -0500

    Fix

--- test/api/cpp/proj-issue487.cpp
+++ test/api/cpp/proj-issue487.cpp
@@ -1,4 +1,20 @@
-#include <cvc5/cvc5.h>
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2021 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Test for project issue #487
+ *
+ */
+
+#include "api/cpp/cvc5.h"
 #include <cassert>
 
 using namespace cvc5::api;
