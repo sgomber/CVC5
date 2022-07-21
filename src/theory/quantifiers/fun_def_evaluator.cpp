7910f636cda463ab06605ec7ef3d7b1ef1b85761
ajreynol <andrew.j.reynolds@gmail.com>
2022-07-21 11:15:02 -0500

    more

--- src/theory/quantifiers/fun_def_evaluator.cpp
+++ src/theory/quantifiers/fun_def_evaluator.cpp
@@ -19,6 +19,7 @@
 #include "options/quantifiers_options.h"
 #include "theory/quantifiers/quantifiers_attributes.h"
 #include "theory/rewriter.h"
+#include "expr/node_algorithm.h"
 
 using namespace cvc5::internal::kind;
 
@@ -57,8 +58,8 @@ Node FunDefEvaluator::evaluateDefinitions(Node n)
   Node curr = n;
   for (size_t i = 0; i < options().quantifiers.sygusRecFunEvalLimit; i++)
   {
-    // rewrite curr
-    curr = rewrite(curr);
+    // expand definitions, with rewriting
+    curr = convert(curr);
     Trace("fd-eval-debug") << "...rewritten (" << i << "):" << curr
                            << std::endl;
     // if constant, we are done
@@ -67,40 +68,42 @@ Node FunDefEvaluator::evaluateDefinitions(Node n)
       Trace("fd-eval") << "...return " << curr << std::endl;
       return curr;
     }
-    // expand definitions
-    curr = convert(curr);
-    Trace("fd-eval-debug") << "...expanded (" << i << "):" << curr << std::endl;
   }
   Trace("fd-eval") << "...failed, return original" << std::endl;
   return n;
 }
 
-bool FunDefEvaluator::shouldTraverse(Node n) { return !n.isClosure(); }
+bool FunDefEvaluator::shouldTraverse(Node n) { return true; }
 
 Node FunDefEvaluator::postConvert(Node n)
 {
   if (n.getKind() != APPLY_UF)
   {
     // not an application of a recursive function
-    return n;
+    return rewrite(n);
   }
   // need to evaluate it
   Node f = n.getOperator();
   std::map<Node, FunDefInfo>::const_iterator itf = d_funDefMap.find(f);
   if (itf == d_funDefMap.end())
   {
-    return n;
+    return rewrite(n);
   }
   // get the function definition
   Node sbody = itf->second.d_body;
   const std::vector<Node>& args = itf->second.d_args;
-  if (!args.empty())
+  std::vector<Node> children(n.begin(), n.end());
+  for (const Node& c : children)
   {
-    std::vector<Node> children(n.begin(), n.end());
-    // invoke it on arguments using the evaluator, which will rewrite
-    sbody = evaluate(sbody, args, children);
-    Assert(!sbody.isNull());
+    if (expr::hasBoundVar(c))
+    {
+      // do not evaluate on terms with bound variables, which can lead to variable shadowing issues
+      return rewrite(n);
+    }
   }
+  // invoke it on arguments using the evaluator, which will rewrite
+  sbody = evaluate(sbody, args, children);
+  Assert(!sbody.isNull());
   return sbody;
 }
 
