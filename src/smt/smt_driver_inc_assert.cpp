3db3c889e2a93a7305a15b1ddb7fdb86ad19e963
ajreynol <andrew.j.reynolds@gmail.com>
2022-08-25 14:29:37 -0500

    Incremental assert version

---
+++ src/smt/smt_driver_inc_assert.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds, Aina Niemetz, Morgan Deters
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The solver for SMT queries in an SolverEngine.
+ */
+
+#include "smt/smt_driver_inc_assert.h"
+
+#include "options/base_options.h"
+#include "prop/prop_engine.h"
+#include "smt/env.h"
+#include "smt/smt_solver.h"
+
+namespace cvc5::internal {
+namespace smt {
+
+SmtDriverIncAssert::SmtDriverIncAssert(Env& env,
+                                             SmtSolver& smt,
+                                             ContextManager* ctx)
+    : SmtDriver(env, smt, ctx), d_initialized(false)
+{
+}
+
+Result SmtDriverIncAssert::checkSatNext(bool& checkAgain)
+{
+  Assertions& as = d_smt.getAssertions();
+  d_smt.preprocess(as);
+  if (!d_initialized)
+  {
+    d_ppAsserts = d_smt.getPreprocessedAssertions();
+    d_ppSkolemMap = d_smt.getPreprocessedSkolemMap();
+    checkAgain = true;
+    d_initialized = true;
+    return Result();
+  }
+  d_smt.assertToInternal(as);
+  Result result = d_smt.checkSatInternal();
+  return result;
+}
+
+void SmtDriverIncAssert::getNextAssertions(Assertions& as)
+{
+}
+
+}  // namespace smt
+}  // namespace cvc5::internal
---
+++ src/smt/smt_driver_inc_assert.h
@@ -0,0 +1,50 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * The solver for SMT queries in an SolverEngine.
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__SMT__SMT_DRIVER_INC_ASSERT_H
+#define CVC5__SMT__SMT_DRIVER_INC_ASSERT_H
+
+#include "smt/smt_driver.h"
+#include "util/result.h"
+
+namespace cvc5::internal {
+namespace smt {
+
+class SmtSolver;
+class ContextManager;
+
+class SmtDriverIncAssert : public SmtDriver
+{
+ public:
+  SmtDriverIncAssert(Env& env, SmtSolver& smt, ContextManager* ctx);
+
+ protected:
+  Result checkSatNext(bool& checkAgain) override;
+  void getNextAssertions(Assertions& as) override;
+ private:
+   /** Initialized */
+   bool d_initialized;
+   /** The original preprocessed assertions */
+   std::vector<Node> d_ppAsserts;
+   /** The original skolem map */
+  std::unordered_map<size_t, Node> d_ppSkolemMap;
+};
+
+}  // namespace smt
+}  // namespace cvc5::internal
+
+#endif
