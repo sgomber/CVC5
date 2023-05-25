72a72a548f6ac5986080c12f7568417fb1d56bc4
ajreynol <andrew.j.reynolds@gmail.com>
2023-05-25 12:21:17 -0500

    Add

---
+++ src/main/lemma_saver.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Lemma saver for cvc5.
+ */
+
+#include "main/lemma_saver.h"
+
+#include "base/check.h"
+#include "base/output.h"
+
+namespace cvc5 {
+namespace main {
+
+LemmaSaver::LemmaSaver(std::string& filename,
+                         Solver* s)
+    : d_filename(filename), d_solver(s)
+{
+  d_fs.open(filename, std::fstream::out);
+}
+LemmaSaver::~LemmaSaver() {
+  d_fs.close();
+}
+
+void LemmaSaver::notifyLemma(const Term& t)
+{
+  Trace("lemma-saver") << "LemmaSaver::notifyLemma " << t << std::endl;
+  d_fs << t.toString() << std::endl;
+}
+
+std::string LemmaSaver::getName() { return "LemmaSaver"; }
+
+std::string LemmaSaver::getFileName() { return d_filename; }
+
+}  // namespace main
+}  // namespace cvc5
---
+++ src/main/lemma_saver.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Lemma saver for cvc5.
+ */
+
+#ifndef CVC5__LEMMA_SAVER_H
+#define CVC5__LEMMA_SAVER_H
+
+#include <cvc5/cvc5.h>
+
+#include <fstream>
+
+namespace cvc5 {
+namespace main {
+
+/**
+ * The lemma saver plugin, for saving lemmas to disk.
+ */
+class LemmaSaver : public cvc5::Plugin
+{
+ public:
+  LemmaSaver(std::string& filename, Solver* s);
+  ~LemmaSaver();
+  /** */
+  void notifyLemma(const Term& t) override;
+  /** Get name, for debugging */
+  std::string getName() override;
+  /** The filename we are saving to */
+  std::string getFileName();
+
+ private:
+  /** The filename to read from */
+  std::string d_filename;
+  /** The solver we are associated with */
+  Solver* d_solver;
+  /** File stream */
+  std::fstream d_fs;
+};
+
+}  // namespace main
+}  // namespace cvc5
+
+#endif /* CVC5__LEMMA_SAVER_H */
